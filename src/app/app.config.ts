26808eca78bf7d26a1e2f4a073c527919aedf983
Jose Miguel Paniagua Tinajero <ttn.developer@gmail.com>
2024-10-13 07:06:09 -0600

    -patching: Maximum budget incremented. Environments files.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,15 +5,15 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { developmentEnvironment } from './environments/environment.development';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { firebaseConfig } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideFirebaseApp(() => initializeApp(developmentEnvironment)),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } },
   ]
--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,9 +1,9 @@
 /*
- Copy this file in folder ./environments and rename to environment.ts, then change the variable name
- to productionEnvironment and type your firebase credentials. You also type them in this file.
+ Copy this file in folder ./environments and rename to environment.ts, then
+ type your firebase credentials. You also type them in this file.
  */
 
-export const developmentEnvironment = {
+export const firebaseConfig = {
   "projectId":"YOUR_KEY",
   "appId":"YOUR_KEY",
   "storageBucket":"YOUR_KEY",
